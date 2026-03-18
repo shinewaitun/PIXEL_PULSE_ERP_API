57828a6c93e4ee797ee2d2dbaeb5713bbe248a41
ShineWaiTun <shinewaitun49@gmail.com>
2026-03-18 14:53:45 +0700

    pakage and seeders

--- src/database/migrations/1773820356381-CreateUserTable.ts
+++ src/database/migrations/1773820356381-CreateUserTable.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUserTable1773819049782 implements MigrationInterface {
-    name = 'CreateUserTable1773819049782'
+export class CreateUserTable1773820356381 implements MigrationInterface {
+    name = 'CreateUserTable1773820356381'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`users\` (\`id\` varchar(36) NOT NULL, \`isActive\` tinyint NOT NULL DEFAULT 1, \`createdAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deletedAt\` timestamp(6) NULL, \`apiKey\` varchar(255) NULL, \`username\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`phoneNumber\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`status\` enum ('ACTIVE', 'INACTIVE') NOT NULL DEFAULT 'ACTIVE', \`role\` enum ('Admin', 'Staff', 'Recruiter', 'Candidate') NOT NULL DEFAULT 'Staff', \`subscription\` enum ('Unpaid', 'Basic', 'Premium') NOT NULL DEFAULT 'Unpaid', \`created_by\` varchar(36) NULL, \`updated_by\` varchar(36) NULL, \`deleted_by\` varchar(36) NULL, UNIQUE INDEX \`IDX_c654b438e89f6e1fbd2828b5d3\` (\`apiKey\`), UNIQUE INDEX \`IDX_fe0bb3f6520ee0469504521e71\` (\`username\`), UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`), UNIQUE INDEX \`IDX_1e3d0240b49c40521aaeb95329\` (\`phoneNumber\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
